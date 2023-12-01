690666adb106ec75e86a14e368735bef5fc2071a
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2023-12-01 09:49:59 -0500

    AppRouterr.jsx created and basic features added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CalendarApp } from './CalendarApp'
+import { AppRouter } from './router/AppRouter'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <CalendarApp />
-  
+	<AppRouter>
+    	<CalendarApp />
+    </AppRouter>
 )
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import React from 'react'
+
+import { CalendarScreen } from '../views/CalendarScreen'
+import { LoginScreen } from '../views/LoginScreen'
+
+export const AppRouter = () => {
+  return (
+    
+    <BrowserRouter>
+
+        <Routes>
+
+            <Route path='/' element={ <CalendarScreen /> } />
+            <Route path='/login' element={ <LoginScreen /> } />
+            <Route path='/*' element = { <Navigate to='/' /> } />
+
+        </Routes>
+    
+    </BrowserRouter>
+
+  )
+}
