fbae52836681ca5bcb36304d307da10380e3e481
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2024-01-04 11:11:42 -0500

    Routes protection added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import React, { useEffect } from 'react';
+import ReactLoading from 'react-loading'
 
 import { CalendarScreen } from '../views/CalendarScreen'
 import { LoginScreen } from '../views/LoginScreen'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startChecking } from '@/store/auth/thunks';
+import PrivateRoutes from './PrivateRoutes';
+import { PublicRoutes } from './PublicRoutes';
+
+import '../css/react-loading.css'
 
 export const AppRouter = () => {
 
   const dispacth = useDispatch();
+  const { checking } = useSelector( state => state.auth )
   
   useEffect(() => {
     
@@ -16,17 +22,37 @@ export const AppRouter = () => {
     
   }, [])
   
-  
+	if (checking) {
+		
+		return (
+			<div className='react-loading' >
+				<ReactLoading type='spin' color='#0000FF' height={80} width={80} />
+			</div>
+		)
+
+	}
 
   return (
     
     <BrowserRouter>
 
+
+
         <Routes>
 
-            <Route path='/' element={ <CalendarScreen /> } />
-            <Route path='/login' element={ <LoginScreen /> } />
-            <Route path='/*' element = { <Navigate to='/' /> } />
+            <Route path='/' element={ 
+				<PrivateRoutes>
+					<CalendarScreen /> 
+				</PrivateRoutes>
+			} />
+            
+            <Route path='/login' element={ 
+				<PublicRoutes>
+					<LoginScreen /> 
+				</PublicRoutes>
+			} />
+            
+			<Route path='/*' element = { <Navigate to='/' /> } />
 
         </Routes>
     
